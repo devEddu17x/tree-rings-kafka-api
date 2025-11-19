8c10ffb671e5aed554e8db5946ec3f91a5535f95
Eddu <eduarjma@gmail.com>
2025-11-19 23:49:04 +0000

    feat: add global validation pipe to enhance request validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.setGlobalPrefix(configService.get('api').prefix);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(configService.get('api').port);
 }
 bootstrap();
