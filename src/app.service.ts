6d849eb4edbf3ea27736afc72b054e4c72fc72e6
Eddu <eduarjma@gmail.com>
2025-11-19 23:24:50 +0000

    feat: rename AppService to HealthStatusService and update response message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class HealthStatusService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API is healthy';
   }
 }
