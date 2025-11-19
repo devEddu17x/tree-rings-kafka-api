9b10d7619349d0d44d0fb6a68ca97ba36be6c0c5
Eddu <eduarjma@gmail.com>
2025-11-19 23:44:22 +0000

    feat: add PORT configuration to API settings

--- src/config/env/api.config.ts
+++ src/config/env/api.config.ts
@@ -1,10 +1,11 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("api", () => {
-  const { API_PREFIX } = process.env;
-
+  const { API_PREFIX, PORT } = process.env;
+  
   return {
     prefix: API_PREFIX || 'api/v1',
+    port: Number(PORT) || 3000,
   };
 
 } );
