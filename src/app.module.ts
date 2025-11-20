b7f54419b1d88c4e5d8a8c0c91b5e20712afa0b7
Eddu <azabacheeduardo85@gmail.com>
2025-11-20 10:51:50 -0500

    feat: add StorageModule to the application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HealthStatusController } from './app.controller';
 import { HealthStatusService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { StorageModule } from './storage/storage.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, StorageModule],
   controllers: [HealthStatusController],
   providers: [HealthStatusService],
 })
