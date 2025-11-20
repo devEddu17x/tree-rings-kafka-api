cd35f19a8ca1669afa6bdc8af47a7bf54264707e
Eddu <azabacheeduardo85@gmail.com>
2025-11-20 13:51:59 -0500

    feat: add AnalysisModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { HealthStatusController } from './app.controller';
 import { HealthStatusService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { StorageModule } from './storage/storage.module';
+import { AnalysisModule } from './analysis/analysis.module';
 
 @Module({
-  imports: [ConfigModule, StorageModule],
+  imports: [ConfigModule, StorageModule, AnalysisModule],
   controllers: [HealthStatusController],
   providers: [HealthStatusService],
 })
