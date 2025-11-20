5879dbb6f9f718c626eb400c4873c84bc3e54349
Eddu <azabacheeduardo85@gmail.com>
2025-11-20 17:48:16 -0500

    feat: export kafkaConfig in env index file

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,3 +1,4 @@
 export { default as apiConfig } from './api.config'
 export { default as storageConfig } from './storage.config'
 export { default as processConfig } from './process.config'
+export { default as kafkaConfig } from './kafka.config'
