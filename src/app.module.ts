55c6ed3c88150ddb2b4cc273b8fbde3c68cacb51
Eddu <azabacheeduardo85@gmail.com>
2025-11-20 14:00:38 -0500

    feat: add KafkaModule and KafkaService for message handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { HealthStatusService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { StorageModule } from './storage/storage.module';
 import { AnalysisModule } from './analysis/analysis.module';
+import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
-  imports: [ConfigModule, StorageModule, AnalysisModule],
+  imports: [ConfigModule, StorageModule, AnalysisModule, KafkaModule],
   controllers: [HealthStatusController],
   providers: [HealthStatusService],
 })
