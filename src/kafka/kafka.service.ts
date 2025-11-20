b21313690ff4620b283583a63c7331f22f5312bb
Eddu <azabacheeduardo85@gmail.com>
2025-11-20 17:50:50 -0500

    feat: implement KafkaService with Kafka configuration and producer initialization

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Kafka, Producer } from 'kafkajs';
 
 @Injectable()
-export class KafkaService {}
+export class KafkaService {
+    private kafka: Kafka;
+    private producer: Producer;
+    constructor(private readonly configService: ConfigService) {
+        const kafkaConfig = this.configService.get('kafka');
+        this.kafka = new Kafka(kafkaConfig);
+    }
+}
