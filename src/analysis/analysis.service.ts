384d4d63c86e4d834b64ee70958dd49a3e0e53c4
Eddu <azabacheeduardo85@gmail.com>
2025-11-20 23:08:20 -0500

    feat(analysis): implement image queuing for processing with Kafka integration

--- src/analysis/analysis.service.ts
+++ src/analysis/analysis.service.ts
@@ -1,22 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { StorageService } from 'src/storage/storage.service';
 import { RequestUploadDTO } from './dtos/request-upload.dto';
 import { FilePlan } from 'src/storage/interfaces/file-plan.interface';
 import { ConfigService } from '@nestjs/config';
+import { IngestionPayload } from 'src/kafka/interfaces/kafka-payload.interface';
+import { KafkaService } from 'src/kafka/kafka.service';
+import { randomUUID } from 'crypto';
+import { StartProcessDTO } from './dtos/start-process.dto';
 
 @Injectable()
 export class AnalysisService {
     private ingestionPath: string;
+    private ingestionTopic: string;
+    private readonly logger = new Logger(AnalysisService.name);
     constructor(
         private readonly storageService: StorageService,
         private readonly configService: ConfigService,
+        private readonly kafkaService: KafkaService,
     ) {
         this.ingestionPath = this.configService.get('process').ingestionPath;
+        this.ingestionTopic = this.configService.get('kafka').topics.ingestion;
     }
 
     async getPresignedUploadUrls(requestUploadDto: RequestUploadDTO) {
         const files: FilePlan[] = requestUploadDto.images
         const options = { prefix: this.ingestionPath };
         return this.storageService.generatePresignedUrl(files, options)
+    }
+
+    async queueImagesForProcessing(startProcessDto: StartProcessDTO) {
+        try {
+            const jobId = randomUUID();
+            const promises = startProcessDto.imagesUrl.map(async (imageUrl) => {
+                const payload: IngestionPayload = {
+                    jobId,
+                    file: imageUrl,
+                    timestamp: new Date().toISOString(),
+                };
+                const messageKey = randomUUID();
+                return this.kafkaService.emit(this.ingestionTopic, messageKey, payload);
+            })
+            const queuedEvent = await Promise.all(promises);
+            this.logger.log('queuedEvent:', queuedEvent);
+            return {
+                jobId,
+                status: 'QUEUED',
+                message: `${startProcessDto.imagesUrl.length} images have been queued for processing.`,
+            }
+        } catch (error) {
+            this.logger.error(`Failed to queue images for processing: ${error}`);
+            throw new InternalServerErrorException('Failed to queue images for processing.')
+        }
     }
 }
