45caead970eaa459c44c5832aae8156a419ee5cd
Eddu <azabacheeduardo85@gmail.com>
2025-11-21 17:29:29 -0500

    feat(kafka): import NotificationsModule in KafkaModule for notification handling

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KafkaService } from './kafka.service';
 import { ConsumerService } from './consumer.service';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   providers: [KafkaService, ConsumerService],
   exports: [KafkaService],
 })
