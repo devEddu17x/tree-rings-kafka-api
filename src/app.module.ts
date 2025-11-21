bae71f6b49a2a8c9784138657496b32e5180882c
Eddu <azabacheeduardo85@gmail.com>
2025-11-21 16:06:42 -0500

    feat(app): integrate NotificationsModule and NotificationsGateway into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { ConfigModule } from './config/config.module';
 import { StorageModule } from './storage/storage.module';
 import { AnalysisModule } from './analysis/analysis.module';
 import { KafkaModule } from './kafka/kafka.module';
+import { NotificationsGateway } from './notifications/notifications.gateway';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [ConfigModule, StorageModule, AnalysisModule, KafkaModule],
+  imports: [ConfigModule, StorageModule, AnalysisModule, KafkaModule, NotificationsModule],
   controllers: [HealthStatusController],
-  providers: [HealthStatusService],
+  providers: [HealthStatusService, NotificationsGateway],
 })
 export class AppModule {}
