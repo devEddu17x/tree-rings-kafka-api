2903693fe3f38a1b3fd14a739e2a33e936437940
Eddu <azabacheeduardo85@gmail.com>
2025-11-21 02:37:14 -0500

    fix(analysis): refactor StartProcessDTO to use ImageProcessDTO and update validation

--- src/analysis/dtos/start-process.dto.ts
+++ src/analysis/dtos/start-process.dto.ts
@@ -1,8 +1,23 @@
-import { IsArray, IsNotEmpty, IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, Min, ValidateNested } from "class-validator";
+
+export class ImageProcessDTO {
+    @IsString()
+    @IsNotEmpty()
+    key: string;
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
+    coordinatesX: number;
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
+    coordinatesY: number;
+}
 
 export class StartProcessDTO {
     @IsArray()
-    @IsString({ each: true })
-    @IsNotEmpty({ each: true })
-    imagesUrl: string[];
+    @ValidateNested({ each: true })
+    @Type(() => ImageProcessDTO)
+    images: ImageProcessDTO[];
 }
