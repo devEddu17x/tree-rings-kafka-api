056c37d079356dfbb5e3c90c4adf7977fa8ed0f7
Eddu <azabacheeduardo85@gmail.com>
2025-11-21 16:03:22 -0500

    feat(kafka): add ConsumerService to KafkaModule providers

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { KafkaService } from './kafka.service';
+import { ConsumerService } from './consumer.service';
 
 @Module({
-  providers: [KafkaService],
+  providers: [KafkaService, ConsumerService],
   exports: [KafkaService],
 })
 export class KafkaModule { }
