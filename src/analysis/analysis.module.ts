b4db45bb0f60460805ec46cf4a312faf85c60708
Eddu <azabacheeduardo85@gmail.com>
2025-11-21 00:40:32 -0500

    feat(analysis): add KafkaModule to analysis module imports

--- src/analysis/analysis.module.ts
+++ src/analysis/analysis.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AnalysisController } from './analysis.controller';
 import { AnalysisService } from './analysis.service';
 import { StorageModule } from 'src/storage/storage.module';
+import { KafkaModule } from 'src/kafka/kafka.module';
 
 @Module({
-  imports: [StorageModule],
+  imports: [StorageModule, KafkaModule],
   controllers: [AnalysisController],
   providers: [AnalysisService]
 })
