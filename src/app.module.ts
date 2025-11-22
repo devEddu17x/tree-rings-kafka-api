32df392aa9d430bf137da178cf59d849dfd4c341
Eddu <azabacheeduardo85@gmail.com>
2025-11-22 10:25:05 -0500

    refactor(app): remove NotificationsGateway from providers for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,11 @@ import { ConfigModule } from './config/config.module';
 import { StorageModule } from './storage/storage.module';
 import { AnalysisModule } from './analysis/analysis.module';
 import { KafkaModule } from './kafka/kafka.module';
-import { NotificationsGateway } from './notifications/notifications.gateway';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [ConfigModule, StorageModule, AnalysisModule, KafkaModule, NotificationsModule],
   controllers: [HealthStatusController],
-  providers: [HealthStatusService, NotificationsGateway],
+  providers: [HealthStatusService],
 })
 export class AppModule { }
